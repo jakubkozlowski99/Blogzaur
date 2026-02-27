bb9d4e6f2d4d4888da537784da6b29ff5d6041cb
jkozlowski757 <jkozlowski757@gmail.com>
2026-02-27 13:45:10 +0100

    Added home page

--- Blogzaur.MVC/Controllers/HomeController.cs
+++ Blogzaur.MVC/Controllers/HomeController.cs
@@ -1,20 +1,39 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Blogzaur.MVC.Models;
+using MediatR;
+using Blogzaur.Application.BlogEntry.Queries.GetAllBlogEntries;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Blogzaur.MVC.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IMediator _mediator;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IMediator mediator)
     {
         _logger = logger;
+        _mediator = mediator;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        // Load all blog entries and prepare trending lists for the view
+        var all = (await _mediator.Send(new GetAllBlogEntriesQuery())).ToList();
+
+        ViewBag.TrendingMostLiked = all
+            .OrderByDescending(b => b.LikeAmount)
+            .Take(3)
+            .ToList();
+
+        ViewBag.TrendingMostViewed = all
+            .OrderByDescending(b => b.Views)
+            .Take(3)
+            .ToList();
+
         return View();
     }
 
